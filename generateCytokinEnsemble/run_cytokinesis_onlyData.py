483409de31a52230d7776acb7ba0b9ff88f859c6
unknown <matthias.haering@ds.mpg.de>
2024-03-26 16:28:51 +0100

    python scripte huebscher machen

--- generateCytokinEnsemble/run_cytokinesis_onlyData.py
+++ generateCytokinEnsemble/run_cytokinesis_onlyData.py
@@ -2,26 +2,17 @@
 
 from __future__ import division
 import numpy as np
-import pylab as plt
 import os
-import scipy.special as sp
-#import scipy.stats as st
-
-import sys
 import pickle
 
-
-class PureDriftAndDiffusion(object):
+class generateCyto(object):
     
     def __init__(self,fit_until_this_t=14,nrealiz=5):
-
 
         self.fit_until_this_t = fit_until_this_t
         self.nrealiz=nrealiz
 
-
-
-        #####################################
+        ##################################### fixed parameters here, variable parameters are passed in run()
         self.seed = 123456
         self.var_start_sim = 0
         self.Tabsorb = 0
@@ -54,19 +45,7 @@ class PureDriftAndDiffusion(object):
         self.gamma1pre = 2*self.K*self.xi*10
         self.gamma1 = 0
         self.gamma2pre = 20*2*np.pi*self.A*self.xi*self.Nb 
-        self.gamma2 = 20*2*np.pi*self.A*self.xi*self.Nb 
-        
-        ### paper prameters ####
-        """
-        kd = 0.1 1/sec
-        kp = 2.5 1/(s mu m)
-        Nb = 40
-        A = 1.2*10^3 nN mum^2
-        xi = 3.75 10^-4 mum
-        c0 = 25 1/mum
-        K = 10 nN mum^-1
-        R0 = 14.5 mum
-        """        
+        self.gamma2 = 20*2*np.pi*self.A*self.xi*self.Nb      
         
     def run(self,params):
         kp1,kd1,kp,kd,alpha = params
@@ -96,89 +75,33 @@ class PureDriftAndDiffusion(object):
         self.gamma1,self.gamma2,self.alpha,self.kp1,self.kd1,self.gamma1pre,self.gamma2pre))  
         ########################################### 
         
-        datafile_hittingT='hittingTime.txt'
-        self.hitting_times = np.genfromtxt(datafile_hittingT)
-        self.x_start_mean_values = np.ones(self.nrealiz)*self.x_start_sim
-        
+        ##### load data into class for later saving
         numfile_x = 'num_trj.txt'
         x_num = np.loadtxt(numfile_x, unpack=1)
         
         self.x_data = x_num
         
-        numfile_mean = 'num_mean.txt'
-        self.mean = np.loadtxt(numfile_mean, unpack=1)
-
-        numfile_var = 'num_var.txt'
-        self.var = np.loadtxt(numfile_var, unpack=1)
-        
-        numfile_skew = 'num_skew.txt'
-        self.skew = np.loadtxt(numfile_skew, unpack=1)         
-
-        numfile_meanConc = 'num_meanConc.txt'
-        self.meanConc = np.loadtxt(numfile_meanConc, unpack=1)  
-        
-        numfile_conc = 'num_conc.txt'
-        self.conc = np.loadtxt(numfile_conc, unpack=1)
-        
-
-
-
-
 
 
 #########################  M  A I  N  ########################################
 ##############################################################################
 ##############################################################################
 
-bool_init = True
-bool_run = True
+### instantiate class
+nrealiz = 10000
+names = ['drift','neg_two']#['drift','bessel','neg_two']
+#kp1,kd1,kp,kd,alpha
+p0 = [400,10,2000,20,1]
+p1 = [0.5,0.01,0,0,1]
+p2 = [0.7,0.1,0,0,2]
+all_params = [p0,p2] #[p0,p1,p2]
 
+gC = generateCyto(fit_until_this_t=300,nrealiz=nrealiz) 
 
-if(bool_init):
-
-
-    nrealiz = 10000
-    
-    names = ['drift','neg_two']#['drift','bessel','neg_two']
-    #kp1,kd1,kp,kd,alpha
-    p0 = [400,10,2000,20,1]
-    p1 = [0.5,0.01,0,0,1]
-    p2 = [0.7,0.1,0,0,2]
-    all_params = [p0,p2] #[p0,p1,p2]
-
-    ##########################################################################    
-    # Instantiate Classes###################################################  
-    ##########################################################################  
-    PD = PureDriftAndDiffusion(fit_until_this_t=300,nrealiz=nrealiz) #fit_until_this_t=1000
-
-
-    """
-    kp1= params[0]
-    kd1= params[1]
-    kp = params[2]
-    kd = params[3]
-    alph = params[4] 
-
-    if(name=='bessel'):
-        gam=PD.gamma2*PD.rc_fac**alph
-
-    if(process=='NESS_drift'):
-        gam = PD.gamma2*(kp/kd)**alph - PD.gamma1*PD.r0
-
-    if(process=='NESS_neg_two'):
-        gam=PD.gamma2*PD.rc_fac**alph
-    """
-
-            
-
-    ##########################################################################
-    # Produce Data from Classes ###############################################
-    ##########################################################################
-if(bool_run):
-
-    for params,name in zip(all_params,names):
-        PD.run(params)
-        save_name = "x_"+name+"highres10000.pickle"
-        pickle.dump(PD.x_data,open(save_name,"wb"))    
+### produce data
+for params,name in zip(all_params,names):
+    gC.run(params)
+    save_name = "x_"+name+"highres10000.pickle"
+    pickle.dump(gC.x_data,open(save_name,"wb"))    
 
 
--- generatePowerlawEnsemble/GeneratePowerEnsembleForCyto.py
+++ generatePowerlawEnsemble/GeneratePowerEnsembleForCyto.py
@@ -1,22 +1,16 @@
 #!/usr/bin/env python2
 # -*- coding: utf-8 -*-
 
-
-from __future__ import absolute_import, unicode_literals,division#, print_function
+from __future__ import absolute_import, unicode_literals,division
 import numpy as np
-import scipy as sp
-
 import os
 
 ###############################################################################
-
-
-## need to set parameters in this class
-class PureFwdPowerlaw(object):
+class generatePowerEnsemble(object):
 
     def __init__(self,fit_until_this_t=300,nrealiz=5,gamma=1.0,D=0.2,alpha=-1.0,beta=0.0):
-
-
+        
+        ##################################### these parameters are passed as argument
         self.fit_until_this_t = fit_until_this_t
         self.nrealiz=nrealiz
         self.gamma = gamma
@@ -24,17 +18,14 @@ class PureFwdPowerlaw(object):
         self.alpha = alpha
         self.beta = beta
 
-
-
-        #####################################
+        ##################################### need to set these parameters in this class
         self.var_start_sim = 0
-        self.x_upper_bound = 1000 #3
+        self.x_upper_bound = 1000 
         self.max_rtime = fit_until_this_t
-        self.traFreq = 1#100
-        self.wfreq = 50#100#100
-        self.total_time=2000#80#max(40,fit_until_this_t)
+        self.traFreq = 1
+        self.wfreq = 50
+        self.total_time=2000
         self.x0_for_reverse = 0
-        #self.D = 0.2
         self.dt = 0.005
         self.dt_eff = self.dt*self.traFreq
         self.seed = 12345
@@ -42,7 +33,6 @@ class PureFwdPowerlaw(object):
 
 
     def run(self):
-
         ###########################################      
         os.system('./src_5_pureFwdPowerlaw_notrj/prog %f %d %d %f %f %f %d %d %d %d %f %f %f %f %f' % (
         self.x_start_sim,self.nrealiz,self.seed,self.dt,self.D,
@@ -54,50 +44,35 @@ class PureFwdPowerlaw(object):
         
          
 ##############################################################################
-#gamma,D,alpha,beta
-params=[1.0,0.2,-1.0,0.0]
 fit_until_this_t=30
-fit_until_this_t_ref = fit_until_this_t
 nrealiz = 2000
-gamma,D,alpha,beta = params
-max_load_fwd = 50
-t_theory = np.linspace(0,3,300)
-x0 = 1 ### ATTENTION: must be set above in the c++ simulation, this one is only for saving the data and must be set manually
 
-#gamma = 0.16228566887687898
+alpha_ar = [1]#[0.0, -1.0, -2.0]
+gamma_ar = [1]#[0.03, 0.31, 2.16]
+D_ar = [0.02]#[0.04,0.04,0.04]
 
-simulationkey = True 
-evaluationkey = False 
+for i in np.arange(len(alpha_ar)):
+    alpha = alpha_ar[i]
+    gamma = gamma_ar[i]
+    D = D_ar[i]
 
+    print('#####################################')
+    print('alpha = ', alpha, ' ,gamma = ', gamma, ',D = ', D)
+    print('#####################################')
+    gPE = generatePowerEnsemble(fit_until_this_t,nrealiz,gamma,D,alpha)
+    gPE.run()
 
-if simulationkey == True:
-    alpha_ar = [1]#[0.0, -1.0, -2.0]
-    gamma_ar = [1]#[0.03, 0.31, 2.16]
-    D_ar = [0.02]#[0.04,0.04,0.04]
-
-    for i in np.arange(len(alpha_ar)):
-        alpha = alpha_ar[i]
-        gamma = gamma_ar[i]
-        D = D_ar[i]
-
-        #gamma = 0.0
-        print('#####################################')
-        print('alpha = ', alpha, ' ,gamma = ', gamma, ',D = ', D)
-        print('#####################################')
-        PFP = PureFwdPowerlaw(fit_until_this_t,nrealiz,gamma,D,alpha,beta)
-        PFP.run()
-
-        '''
-        # load output of c++ simulation and save in python
-        # can also load the raw trajectories and more, for info look into c++ code. the 'num_...' files are the c++ outputs. their naming should already be telling as well.
-        md = np.loadtxt('num_mean.txt')
-        mv = np.loadtxt('num_var.txt')
-        cov_ar=np.loadtxt("num_cov.txt")
-        #hittingTime=np.loadtxt("hittingTime.txt")
-        
-        np.savetxt("meannegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), md)
-        np.savetxt("varnegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), mv)
-        np.savetxt("covnegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), cov_ar)
-        '''
+    '''
+    # load output of c++ simulation and save in python
+    # can also load the raw trajectories and more, for info look into c code. the 'num_...' files are the c outputs. their naming should already be telling as well.
+    md = np.loadtxt('num_mean.txt')
+    mv = np.loadtxt('num_var.txt')
+    cov_ar=np.loadtxt("num_cov.txt")
+    #hittingTime=np.loadtxt("hittingTime.txt")
+    
+    np.savetxt("meannegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), md)
+    np.savetxt("varnegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), mv)
+    np.savetxt("covnegtwo_gamma{}_D{}_alpha{}_beta{}_N{}.txt".format(int(gamma*10),int(D*10), int(alpha*10), int(beta*10), nrealiz), cov_ar)
+    '''
 
 
